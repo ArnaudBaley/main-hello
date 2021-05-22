86cdb14a610736eeea3cf61ae3193532a883ba39
Arnaud <>
2021-05-22 08:36:21 +0200

    2021_05_22_08_36_20

--- hello-pwa/js/main.js
+++ hello-pwa/js/main.js
@@ -9,8 +9,7 @@ window.onload = () => {
     requestNotifPermission();
 }
 
-const games = [
-    {
+const games = [{
         slug: 'lost-in-cyberspace',
         name: 'Lost in Cyberspace',
         author: 'Zosia and Bartek',
@@ -37,23 +36,28 @@ const games = [
 ];
 
 function requestNotifPermission() {
-    Notification.requestPermission().then(function(result) {
-        if(result === 'granted') {
+    Notification.requestPermission().then(function (result) {
+        if (result === 'granted') {
             randomNotification();
         }
     });
 }
 
 function randomNotification() {
-    var randomItem = Math.floor(Math.random()*games.length);
+    var randomItem = Math.floor(Math.random() * games.length);
     var notifTitle = games[randomItem].name;
-    var notifBody = 'Créé par '+games[randomItem].author+'.';
-    var notifImg = 'data/img/'+games[randomItem].slug+'.jpg';
+    var notifBody = 'Créé par ' + games[randomItem].author + '.';
+    var notifImg = 'data/img/' + games[randomItem].slug + '.jpg';
     var options = {
         body: notifBody,
         icon: notifImg
     }
-    var notif = new Notification(notifTitle, options);
+
+    navigator.serviceWorker.ready.then(registration => {
+        registration.showNotification('Hello world!', options);
+    })
+
+    // var notif = new Notification(notifTitle, options);
     // self.registration.showNotification('Hello world!', options)
     setTimeout(randomNotification, 5000);
 }
